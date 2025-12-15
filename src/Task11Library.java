5964aeaa15a033e1788d590d9514d7df4604d27e
Isaac Alexander <Isaacalexander81@gmail.com>
2025-12-15 01:56:03 +0100

    src/Task11Library.java

--- src/Task11Library.java
+++ src/Task11Library.java
@@ -1,4 +1,62 @@
-package PACKAGE_NAME;
+// 11. Write a Java program to create a class called "Library" with a collection
+// of books and methods to add and remove books.
 
 public class Task11Library {
+
+    // Book class to store book details
+    static class Book {
+        String title;
+        String author;
+
+        // Constructor
+        Book(String title, String author) {
+            this.title = title;
+            this.author = author;
+        }
+    }
+
+    // Library class to manage a collection of books
+    static class Library {
+        Book[] books = new Book[10]; // fixed-size collection
+        int count = 0;               // number of books currently stored
+
+        // Method to add a book
+        void addBook(Book book) {
+            if (count < books.length) {
+                books[count] = book;
+                count++;
+                System.out.println("Book added: " + book.title);
+            } else {
+                System.out.println("Library is full. Cannot add more books.");
+            }
+        }
+
+        // Method to remove a book by title
+        void removeBook(String title) {
+            for (int i = 0; i < count; i++) {
+                if (books[i].title.equals(title)) {
+                    System.out.println("Removing book: " + books[i].title);
+
+                    // Shift remaining books to the left
+                    for (int j = i; j < count - 1; j++) {
+                        books[j] = books[j + 1];
+                    }
+
+                    books[count - 1] = null;
+                    count--;
+                    return;
+                }
+            }
+            System.out.println("Book not found: " + title);
+        }
+
+        // Method to display all books
+        void showBooks() {
+            System.out.println("\nBooks in Library:");
+            for (int i = 0; i < count; i++) {
+                System.out.println(books[i].title + " by " + books[i].author);
+            }
+        }
+    }
+
 }
