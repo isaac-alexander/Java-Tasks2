7bbdee7355dc2e1af214653041d76d43540186d8
Isaac Alexander <Isaacalexander81@gmail.com>
2025-12-22 01:59:04 +0100

    src/SavingsAccount.java

--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+// SavingsAccount subclass
+public class SavingsAccount extends BankAccount {
+    double interestRate;
 
-public class SavingsAccount {
+    // Constructor
+    SavingsAccount(int accNo, String name, double bal, double rate) {
+        super(accNo, name, bal);
+        interestRate = rate;
+    }
+
+    // Apply interest
+    void applyInterest() {
+        double interest = balance * interestRate / 100;
+        balance = balance + interest;
+        System.out.println("Interest added: " + interest);
+    }
 }
