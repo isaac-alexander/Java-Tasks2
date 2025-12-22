7a05b9fe2fe29c9b777b550832441256b3aed21e
Isaac Alexander <Isaacalexander81@gmail.com>
2025-12-22 01:59:40 +0100

    src/LoyalCustomer.java

--- src/LoyalCustomer.java
+++ src/LoyalCustomer.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+// Task 22: Loyal customer with discount
 
-public class LoyalCustomer {
+public class LoyalCustomer extends Customer {
+
+    double discountRate; // example: 0.10 means 10% discount
+
+    // Constructor
+    public LoyalCustomer(String name, String email, double discountRate) {
+        super(name, email);
+        this.discountRate = discountRate;
+    }
+
+    // Apply discount to total spending
+    public double getDiscountedTotal() {
+        double total = getTotalSpent();
+        return total - (total * discountRate);
+    }
 }
