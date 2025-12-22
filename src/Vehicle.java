c7e29863c3827483953964384059e5f3632b306e
Isaac Alexander <Isaacalexander81@gmail.com>
2025-12-22 01:59:24 +0100

    src/Vehicle.java

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -3,70 +3,19 @@
 // specific attributes like trunk size for cars and load limit for trucks.
 // Implement a method to display vehicle details in each subclass.
 
-public class Task21Vehicle {
+// Parent class Vehicle
 
-    // Parent class
-    static class Vehicle {
-        String make;
-        String model;
-        int year;
+public class Vehicle {
 
-        // Constructor
-        Vehicle(String mk, String mdl, int yr) {
-            make = mk;
-            model = mdl;
-            year = yr;
-        }
+    String make;
+    String model;
+    int year;
+
+    // Constructor
+    Vehicle(String mk, String mdl, int yr) {
+        make = mk;
+        model = mdl;
+        year = yr;
     }
 
-    // Car subclass
-    static class Car extends Vehicle {
-        double trunkSize;
-
-        // Constructor
-        Car(String mk, String mdl, int yr, double trunk) {
-            super(mk, mdl, yr);
-            trunkSize = trunk;
-        }
-
-        // Display car details
-        void showDetails() {
-            System.out.println("Car Make: " + make);
-            System.out.println("Car Model: " + model);
-            System.out.println("Car Year: " + year);
-            System.out.println("Trunk Size: " + trunkSize + " liters");
-        }
-    }
-
-    // Truck subclass
-    static class Truck extends Vehicle {
-        double loadLimit;
-
-        // Constructor
-        Truck(String mk, String mdl, int yr, double load) {
-            super(mk, mdl, yr);
-            loadLimit = load;
-        }
-
-        // Display truck details
-        void showDetails() {
-            System.out.println("Truck Make: " + make);
-            System.out.println("Truck Model: " + model);
-            System.out.println("Truck Year: " + year);
-            System.out.println("Load Limit: " + loadLimit + " kg");
-        }
-    }
-
-    public static void main(String[] args) {
-
-        // Create Car object
-        Car car1 = new Car("Toyota", "Corolla", 2020, 470);
-        car1.showDetails();
-
-        System.out.println();
-
-        // Create Truck object
-        Truck truck1 = new Truck("Ford", "F-150", 2019, 1200);
-        truck1.showDetails();
-    }
 }
