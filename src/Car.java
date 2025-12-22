3b8499fa20d8ac5fd85021ac8408128d8a414651
Isaac Alexander <Isaacalexander81@gmail.com>
2025-12-22 01:58:46 +0100

    src/Car.java

--- src/Car.java
+++ src/Car.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+// 21. Write a Java program to create a class called "Vehicle" with attributes
+// for make, model, and year. Create subclasses "Car" and "Truck" that add
+// specific attributes like trunk size for cars and load limit for trucks.
+// Implement a method to display vehicle details in each subclass.
 
-public class Car {
+
+// Car subclass
+public class Car extends Vehicle {
+    double trunkSize;
+
+    // Constructor
+    Car(String mk, String mdl, int yr, double trunk) {
+        super(mk, mdl, yr);
+        trunkSize = trunk;
+    }
+
+    // Display car details
+    void showDetails() {
+        System.out.println("Car Make: " + make);
+        System.out.println("Car Model: " + model);
+        System.out.println("Car Year: " + year);
+        System.out.println("Trunk Size: " + trunkSize + " liters");
+    }
 }
