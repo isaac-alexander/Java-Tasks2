357d91c1b60e1f7cbee3ca464f10038c84d0ddbd
Isaac Alexander <Isaacalexander81@gmail.com>
2025-12-23 01:59:08 +0100

    src/WashingMachine.java

--- src/WashingMachine.java
+++ src/WashingMachine.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+// 24.  Create a subclass " WashingMachine" that adds a warranty period attribute and a method to extend the warranty.
 
-public class WashingMachine {
+// Subclass of ElectronicsProduct for Task 24
+public class WashingMachine extends ElectronicsProduct {
+
+    int warrantyPeriod; // in years
+
+    // Constructor
+    public WashingMachine(int productId, String name, double price, int warrantyPeriod) {
+        super(productId, name, price);
+        this.warrantyPeriod = warrantyPeriod;
+    }
+
+    // Extend warranty
+    public void extendWarranty(int extraYears) {
+        warrantyPeriod += extraYears;
+    }
+
+    // Display washing machine details
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Warranty Period: " + warrantyPeriod + " years");
+    }
 }
