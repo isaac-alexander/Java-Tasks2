584d6776d4e6c39e3fb534eef349acbf8ae5b68f
Isaac Alexander <Isaacalexander81@gmail.com>
2025-12-23 01:57:23 +0100

    src/Building.java

--- src/Building.java
+++ src/Building.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+// 25. Write a Java program to create a class called "Building" with attributes for address, number of floors, and total area. Create subclasses "ResidentialBuilding" and "CommercialBuilding" that add specific attributes like number of apartments for residential and office space for commercial buildings. Implement a method to calculate the total rent for each subclass.
+
+// Write a Java program to create a class called "Building"
+// with attributes for address, number of floors, and total area.
 
 public class Building {
+
+    String address;
+    int numberOfFloors;
+    double totalArea;
+
+    // Constructor
+    public Building(String address, int numberOfFloors, double totalArea) {
+        this.address = address;
+        this.numberOfFloors = numberOfFloors;
+        this.totalArea = totalArea;
+    }
+
+    // Display basic building details
+    public void displayDetails() {
+        System.out.println("Address: " + address);
+        System.out.println("Number of Floors: " + numberOfFloors);
+        System.out.println("Total Area: " + totalArea + " sqm");
+    }
 }
