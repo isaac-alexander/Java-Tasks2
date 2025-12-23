2d4e1cc24937facb280838619d392ddb771e7d98
Isaac Alexander <Isaacalexander81@gmail.com>
2025-12-23 01:58:20 +0100

    src/MusicalPerformance.java

--- src/MusicalPerformance.java
+++ src/MusicalPerformance.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+//Create subclass "MusicalPerformance" that add specific attributes like performer list for concerts
 
-public class MusicalPerformance {
+// MusicalPerformance subclass for Task 26
+
+import java.util.ArrayList;
+
+public class MusicalPerformance extends Event {
+
+    ArrayList<String> performers = new ArrayList<>();
+
+    // Constructor
+    public MusicalPerformance(String name, String date, String location) {
+        super(name, date, location);
+    }
+
+    // Add a performer
+    public void addPerformer(String performer) {
+        performers.add(performer);
+    }
+
+    // Display musical performance details
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Performers:");
+        for (String performer : performers) {
+            System.out.println("- " + performer);
+        }
+    }
 }
