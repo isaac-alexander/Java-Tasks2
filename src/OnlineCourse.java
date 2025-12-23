fecd11e93855452386859c15023c320e165d92be
Isaac Alexander <Isaacalexander81@gmail.com>
2025-12-23 01:58:33 +0100

    src/OnlineCourse.java

--- src/OnlineCourse.java
+++ src/OnlineCourse.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+// Create a subclass "OnlineCourse" that adds attributes for platform and duration. Implement methods to display course details and check if the course is eligible for a certificate based on duration
 
-public class OnlineCourse {
+// Subclass OnlineCourse that extends Course
+public class OnlineCourse extends Course {
+
+    String platform;
+    int duration; // duration in hours
+
+    // Constructor
+    public OnlineCourse(String courseName, String instructor, int credits,
+                        String platform, int duration) {
+        super(courseName, instructor, credits);
+        this.platform = platform;
+        this.duration = duration;
+    }
+
+    // Display online course details
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Platform: " + platform);
+        System.out.println("Duration: " + duration + " hours");
+    }
+
+    // Check if eligible for certificate
+    public boolean isEligibleForCertificate() {
+        return duration >= 10;
+    }
 }
