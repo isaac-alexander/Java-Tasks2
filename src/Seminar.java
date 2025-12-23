7936cd981dbc8725a4fd796b20b6710724fcf593
Isaac Alexander <Isaacalexander81@gmail.com>
2025-12-23 01:58:51 +0100

    src/Seminar.java

--- src/Seminar.java
+++ src/Seminar.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+// Create subclass "Seminar" that add specific attributes like number of speakers for seminars
 
-public class Seminar {
+// Seminar subclass for Task 26
+public class Seminar extends Event {
+
+    int numberOfSpeakers;
+
+    // Constructor
+    public Seminar(String name, String date, String location, int numberOfSpeakers) {
+        super(name, date, location);
+        this.numberOfSpeakers = numberOfSpeakers;
+    }
+
+    // Display seminar details
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Number of Speakers: " + numberOfSpeakers);
+    }
 }
