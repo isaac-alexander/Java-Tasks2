229976dee6c68f616e8bf968d6bfba6ad8010ede
Isaac Alexander <Isaacalexander81@gmail.com>
2025-12-23 01:56:49 +0100

    src/CommercialBuilding.java

--- src/CommercialBuilding.java
+++ src/CommercialBuilding.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+// CommercialBuilding subclass for Task 25
 
-public class CommercialBuilding {
+public class CommercialBuilding extends Building {
+
+    double officeSpace;       // in square meters
+    double rentPerSquareMeter;
+
+    // Constructor
+    public CommercialBuilding(
+            String address,
+            int numberOfFloors,
+            double totalArea,
+            double officeSpace,
+            double rentPerSquareMeter
+    ) {
+        super(address, numberOfFloors, totalArea);
+        this.officeSpace = officeSpace;
+        this.rentPerSquareMeter = rentPerSquareMeter;
+    }
+
+    // Calculate total rent
+    public double calculateTotalRent() {
+        return officeSpace * rentPerSquareMeter;
+    }
+
+    // Display commercial building details
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Office Space: " + officeSpace + " sqm");
+        System.out.println("Total Rent: $" + calculateTotalRent());
+    }
 }
+
