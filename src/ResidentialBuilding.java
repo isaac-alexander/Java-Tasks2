3631ed416622541412bf2800d79b77abdf47e767
Isaac Alexander <Isaacalexander81@gmail.com>
2025-12-23 01:57:05 +0100

    src/ResidentialBuilding.java

--- src/ResidentialBuilding.java
+++ src/ResidentialBuilding.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
+//  Create subclasses "ResidentialBuilding" that add specific attributes like number of apartments for residential
 
-public class ResidentialBuilding {
+// ResidentialBuilding subclass for Task 25
+
+public class ResidentialBuilding extends Building {
+
+    int numberOfApartments;
+    double rentPerApartment;
+
+    // Constructor
+    public ResidentialBuilding(
+            String address,
+            int numberOfFloors,
+            double totalArea,
+            int numberOfApartments,
+            double rentPerApartment
+    ) {
+        super(address, numberOfFloors, totalArea);
+        this.numberOfApartments = numberOfApartments;
+        this.rentPerApartment = rentPerApartment;
+    }
+
+    // Calculate total rent
+    public double calculateTotalRent() {
+        return numberOfApartments * rentPerApartment;
+    }
+
+    // Display residential building details
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Apartments: " + numberOfApartments);
+        System.out.println("Total Rent: $" + calculateTotalRent());
+    }
 }
+
