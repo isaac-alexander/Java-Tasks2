8de0d4cf3e1ad6b5f4f519200a4c96ca1f030839
Isaac Alexander <Isaacalexander81@gmail.com>
2025-12-24 02:01:03 +0100

    src/ResortReservation.java

--- src/ResortReservation.java
+++ src/ResortReservation.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+// ResortReservation subclass for Task 28
 
-public class ResortReservation {
+import java.time.LocalDate;
+
+public class ResortReservation extends Reservation {
+
+    int roomNumber;
+
+    // Constructor
+    public ResortReservation(
+            int reservationId,
+            String customerName,
+            LocalDate date,
+            int roomNumber
+    ) {
+        super(reservationId, customerName, date);
+        this.roomNumber = roomNumber;
+    }
+
+    // Change room number
+    public void changeRoom(int newRoomNumber) {
+        this.roomNumber = newRoomNumber;
+    }
+
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Room Number: " + roomNumber);
+    }
 }
