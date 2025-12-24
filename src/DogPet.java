e26a4a72594dfe6f131e42806f313869d7595642
Isaac Alexander <Isaacalexander81@gmail.com>
2025-12-24 02:01:45 +0100

    src/DogPet.java

--- src/DogPet.java
+++ src/DogPet.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+// DogPet subclass for Task 29 (renamed to avoid duplicate Dog class)
 
-public class Dogpet {
+public class DogPet extends Pet {
+
+    String favoriteToy;
+
+    // Constructor
+    public DogPet(String name, int age, String favoriteToy) {
+        super(name, "Dog", age);
+        this.favoriteToy = favoriteToy;
+    }
+
+    // Calculate dog age in human years
+    @Override
+    public int getHumanAge() {
+        return age * 7;
+    }
+
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Favorite Toy: " + favoriteToy);
+        System.out.println("Age in Human Years: " + getHumanAge());
+    }
 }
