c9ee03570b064ed1eec3c7bb617ad1a1d6066bc9
Isaac Alexander <Isaacalexander81@gmail.com>
2025-12-24 02:00:21 +0100

    src/Bird.java

--- src/Bird.java
+++ src/Bird.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+// Bird subclass for Task 29
 
-public class Bird {
+public class Bird extends Pet {
+
+    double wingSpan; // in meters
+
+    // Constructor
+    public Bird(String name, int age, double wingSpan) {
+        super(name, "Bird", age);
+        this.wingSpan = wingSpan;
+    }
+
+    // Calculate bird age in human years (simple logic)
+    @Override
+    public int getHumanAge() {
+        return age * 5;
+    }
+
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Wing Span: " + wingSpan + " meters");
+        System.out.println("Age in Human Years: " + getHumanAge());
+    }
 }
