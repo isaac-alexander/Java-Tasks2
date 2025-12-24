17cd99306cce642fa0518d6c5693f5d7a0ff0a9e
Isaac Alexander <Isaacalexander81@gmail.com>
2025-12-24 02:01:18 +0100

    src/RailwayReservation.java

--- src/RailwayReservation.java
+++ src/RailwayReservation.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+// RailwayReservation subclass for Task 28
 
-public class RailwayReservation {
+import java.time.LocalDate;
+
+public class RailwayReservation extends Reservation {
+
+    String seatNumber;
+
+    // Constructor
+    public RailwayReservation(
+            int reservationId,
+            String customerName,
+            LocalDate date,
+            String seatNumber
+    ) {
+        super(reservationId, customerName, date);
+        this.seatNumber = seatNumber;
+    }
+
+    // Change seat number
+    public void changeSeat(String newSeatNumber) {
+        this.seatNumber = newSeatNumber;
+    }
+
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Seat Number: " + seatNumber);
+    }
 }
